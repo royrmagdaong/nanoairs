4d290f1ea2c7e2d8150829a438dfcb74fe93fdd7
royrmagdaong <royrmagdaong@gmail.com>
2026-07-07 11:11:31 +0800

    fixed bug on date, placed it with props

--- app/components/header-date-container.tsx
+++ app/components/header-date-container.tsx
@@ -3,14 +3,14 @@
 import {useState, useEffect} from "react";
 import moment from "moment"
 export default function HeaderDateContainer() {
-
+    const [dateTimeNow, setDateTimeNow] = useState('')
     useEffect(() => {
-        
+        setDateTimeNow(moment().format('YYYY/MM/DD HH:mm:ss dddd'))
     },[]);
 
   return (
     <nav className="fixed top-3 left-1/3 w-1/3 bg-gray-900 text-white flex flex-col justify-center items-center py-3 text-xs italic" style={{letterSpacing: '1px', background: 'linear-gradient(to right,transparent 0%,rgba(0,0,0,.55) 55%,rgba(0,0,0,.55) 55%,transparent 100%)'}}>
-        {moment().format('YYYY/MM/DD HH:mm:ss dddd')}
+        {dateTimeNow}
     </nav>
   );
 }
