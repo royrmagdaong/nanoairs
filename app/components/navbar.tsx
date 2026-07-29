9aa11ecab9f2753adbd5259d7e4a3155e9fd814e
royrmagdaong <royrmagdaong@gmail.com>
2026-07-29 09:56:17 +0800

    added navbar links

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -37,12 +37,45 @@ export default function NavBar() {
         />
         <span className='text-lg uppercase text-gray-500 font-bold'>Nano Airs</span>
       </div>
-      <div className='ml-4 mt-6'>
+      <div className='ml-4 mt-6 h-full relative'>
         <div onClick={()=>{router.push('/nanoairs/dashboard')}} className={`${pathname.includes('dashboard')?'text-cyan-400 bg-gray-200':''} py-2 hover:text-cyan-400 cursor-pointer hover:bg-gray-200 pl-2`}>Dashboard</div>
         <div onClick={()=>{router.push('/nanoairs/dashboard/raceways-1')}} className={`${activeLink==='raceways-1'?'text-black bg-gray-200 border-l-cyan-400 ':'text-gray-400 border-l-white'} ml-3 py-1 cursor-pointer hover:text-black hover:bg-gray-200 hover:border-l-cyan-400 pl-2 border-l-3`}>Raceways 1</div>
         <div onClick={()=>{router.push('/nanoairs/dashboard/raceways-2')}} className={`${activeLink==='raceways-2'?'text-black bg-gray-200 border-l-cyan-400 ':'text-gray-400 border-l-white'} ml-3 py-1 cursor-pointer hover:text-black hover:bg-gray-200 hover:border-l-cyan-400 pl-2 border-l-3`}>Raceways 2</div>
         <div onClick={()=>{router.push('/nanoairs/dashboard/control-pond-1')}} className={`${activeLink==='control-pond-1'?'text-black bg-gray-200 border-l-cyan-400 ':'text-gray-400 border-l-white'} ml-3 py-1 cursor-pointer hover:text-black hover:bg-gray-200 hover:border-l-cyan-400 pl-2 border-l-3`}>Control Pond 1</div>
         <div onClick={()=>{router.push('/nanoairs/dashboard/control-pond-2')}} className={`${activeLink==='control-pond-2'?'text-black bg-gray-200 border-l-cyan-400 ':'text-gray-400 border-l-white'} ml-3 py-1 cursor-pointer hover:text-black hover:bg-gray-200 hover:border-l-cyan-400 pl-2 border-l-3`}>Control Pond 2</div>
+        
+        <div onClick={()=>{router.push('/nanoairs/dashboard')}} className={`${pathname.includes('users')?'text-cyan-400 bg-gray-200':''} py-2 mt-4 hover:text-cyan-400 cursor-pointer hover:bg-gray-200 pl-2`}>Users</div>
+        <div onClick={()=>{router.push('/nanoairs/dashboard')}} className={`${pathname.includes('settings')?'text-cyan-400 bg-gray-200':''} py-2 hover:text-cyan-400 cursor-pointer hover:bg-gray-200 pl-2`}>Settings</div>
+
+        <div onClick={()=>{router.push('/nanoairs/dashboard')}} className={`${pathname.includes('settings')?'text-cyan-400 bg-gray-200':''} py-2 hover:text-cyan-400 cursor-pointer hover:bg-gray-200 pl-2`}>Logout</div>
+
+        <div>
+          <div className='flex mt-2'>
+            <Image
+              src="/globe.svg"
+              alt="Logo"
+              width={24}
+              height={0}
+              className='ml-1'
+            />
+            <div className='relative' style={{top: '2px', left: '8px'}}>
+              <span className='uppercase text-gray-500 font-bold'>Roy M.</span>
+              <p className='relative bottom-1 text-xs text-gray-400 hover:text-cyan-400 cursor-pointer'>Account Settings</p>
+            </div>
+          </div>
+        </div>
+
+        <div className='bg-cyan-400 ml-2 h-12 w-48 bottom-10 absolute rounded-sm flex items-center justify-center font-bold text-sm text-white hover:text-cyan-400 hover:bg-gray-200 cursor-pointer'>
+          <p>Download Report</p>
+          <Image
+              src="/globe.svg"
+              alt="Logo"
+              width={15}
+              height={0}
+              className='ml-2'
+            />
+        </div>
+
       </div>
     </nav>
   );
