39c61d8a7a818c20d457b2fe29bdc145d5f8f360
royrmagdaong <royrmagdaong@gmail.com>
2026-08-25 09:07:24 +0800

    readjust color - activity logs status badge

--- app/components/status-badge.tsx
+++ app/components/status-badge.tsx
@@ -7,14 +7,14 @@ export default function Status({name}: StatusProps) {
     <div
       className={`outline rounded-sm px-1 py-1 font-medium ${
         name === "Danger"
-          ? "bg-red-200 text-red-700"
+          ? "bg-red-100 text-red-500"
           : name === "Off"
-          ? "bg-red-200 text-red-700"
+          ? "bg-red-100 text-red-500"
           : name === "Warning"
-          ? "bg-orange-200 text-orange-700"
+          ? "bg-yellow-100 text-orange-600"
           : name === "Low"
-          ? "bg-orange-200 text-orange-700"
-          : "bg-green-200 text-green-600"
+          ? "bg-yellow-100 text-orange-600"
+          : "bg-green-100 text-green-600"
       }`}
 
       style={{fontSize: "11px"}}
