a6d52f1caf7c1c13ee0b82a95351d266778d4260
royrmagdaong <royrmagdaong@gmail.com>
2026-08-26 11:34:27 +0800

    ai diagnosis - UI improvements

--- app/components/ai-diagnosis.tsx
+++ app/components/ai-diagnosis.tsx
@@ -1,9 +1,9 @@
 
 export default function AIDiagnosis() {
   return (
-    <div className="p-4">
-      <div>AI Diagnosis</div>
-      <ul className="list-disc ml-10">
+    <div className="px-3 py-2 text-gray-600">
+      <div className="text-sm font-semibold ">AI Diagnosis</div>
+      <ul className="list-disc ml-10 text-sm">
         <li>Water condition is chemically unstable due to low alkalinity, and DO data is unreliable due to sensor error.</li>
         <li>The farm is not in immediate collapse condition, but if alkalinity is not corrected, pH crash risk is high.</li>
       </ul>
